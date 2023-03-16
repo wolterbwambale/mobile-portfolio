af3c7a0038a4abbdeb18f2bb12a26f28dd230535
wolterbwambale <benplaan1@gmail.com>
2023-03-17 00:45:09 +0300

    added main.js file and update index.html file

--- main.js
+++ main.js
@@ -1,192 +1,206 @@
-const navItems = document.querySelector('.nav-menu');
-const openMenu = document.querySelector('.menu-bar');
-
-const closeMenu = document.querySelector('.close-btn');
-const navigationBarClose = document.querySelector('.navbar');
-function show() {
-  navItems.style.display = 'flex';
-  navItems.style.right = '0';
+/* mobile menu */
+const clicked = document.querySelector('#clicked');
+const toolbar = document.querySelector('header');
+const clickedP = document.querySelector('.clicked-p');
+const navLinks = document.querySelectorAll('.clicked-ul li');
+function mobileMenu() {
+  clicked.classList = 'clicked';
+}
+function hideMobileMenu() {
+  clicked.classList = 'not-cliked';
 }
 
-function close() {
-  navItems.style.right = '100%';
-}
-
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
-
-document.querySelectorAll('.item');
-navItems.addEventListener('click', () => {
-  navItems.style.right = '100%';
+toolbar.addEventListener('click', mobileMenu);
+clickedP.addEventListener('click', hideMobileMenu);
+navLinks.forEach((element) => {
+  element.addEventListener('click', hideMobileMenu);
 });
+/* End of mobile menu */
 
+/*  Begin projects */
 const projects = [
-    {
-      id: 1,
-      name: 'Tonic',
-      pic: './images/desktop-images/img1.png',
-      companyId: 'CANOPY',
-      work: 'Back End Dev',
-      date: 2015,
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-      techUse: ['html', 'css', 'javaScript'],
-      projectDemo: 'https://wolterbwambale.github.io/mobile-portfolio//',
-      projectSource: 'https://github.com/Enoisong/Mobile-First-Portfolio.git',
-    },
-  
-    {
-      id: 2,
-      name: 'Multi-Post Stories',
-      pic: './images/desktop-images/img2.png',
-      companyId: 'FACEBOOK',
-      work: 'Full Stack Dev',
-      date: 2015,
-      description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming <br> their friends.',
-      techUse: ['html', 'Ruby on rails', 'css', 'javaScript'],
-      projectDemo: 'https://wolterbwambale.github.io/mobile-portfolio//',
-      projectSource: 'https://github.com/Enoisong/Mobile-First-Portfolio.git',
-    },
-  
-    {
-      id: 3,
-      name: 'Facebook 360',
-      pic: './images/desktop-images/img3.png',
-      companyId: 'FACEBOOK',
-      work: 'Full Stack Dev',
-      date: 2015,
-      description: 'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-      techUse: ['html', 'Ruby on rails', 'css', 'javaScript'],
-      projectDemo: 'https://wolterbwambale.github.io/mobile-portfolio/',
-    },
-  
-    {
-      id: 4,
-      name: 'Uber Navigation',
-      pic: './img/desktop-img/work4-img.svg',
-      companyId: 'Uber',
-      work: 'Lead Developer',
-      date: 2015,
-      description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-      techUse: ['html', 'Ruby on rails', 'css', 'javaScript'],
-      projectDemo: 'https://wolterbwambale.github.io/mobile-portfolio/',
-    },
-  ];
-  const mainContainer = document.getElementById('workscontainer');
-  const popup = document.getElementById('popup');
-  const windowPopup = document.querySelector('.winUp');
-  function popupFunc(id) {
-    if (id === null) return;
-    let oneCard = projects.filter((el) => el.id === id);
-    oneCard = oneCard.shift();
-    const popupContent = ` <div class="container windowPopup popup">
-                  <div class="pop-header">
-                      <h4 class="h-4">${oneCard.name}</h4>
-                      <span class="closePopup" onclick="closePopupFunc()"><i class="fa fa-times"></i></span>
-                      <ul class="pop-description-element">
-                          <li class="element" id="companyID">${oneCard.companyId}</li>
-                          <li class="element">
-                              <img class="bullet" src="./images/bullet.png" alt="bullet" />
-                          </li>
-                          <li class="element" id="workType">${oneCard.work}</li>
-                          <li class="element">
-                              <img class="bullet" src="./images/bullet.png" alt="bullet" />
-                          </li>
-                          <li class="element" id="date">${oneCard.date}</li>
-                      </ul>
-                  </div>
-                  <img class="work-pic" src=${oneCard.pic} alt="landing page view" />
-                  <img class="work-pic pop-img" src=${oneCard.pic} alt="nature image" />
-                  <div class="pop-element">
-                      <p class="description-text">
-                          ${oneCard.description}
-                      </p>
-                      <div class="pop-items">
-                          <ul class="languages">
-                            ${oneCard.techUse.map((el) => `<li class="lang">${el}</li>`).join('')}
-                          </ul>
-                          <div class="bottom-bar"></div>
-                          <div class="navigation-links">
-                             
-                          </div>
-                      </div>
-                  </div>
-              </div>`;
+  {
+    name: 'Tonic',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    descriptions: `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essent`,
+    image: 'images/projects/project-1.png',
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
+    links: { live: '#', source: '#' },
+  },
+  {
+    name: 'Multi-Post Stories',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    descriptions: `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essent`,
+    image: 'images/projects/project-2.png',
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
+    links: { live: '#', source: '#' },
+  },
+  {
+    name: 'Tonic',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    descriptions: `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essent`,
+    image: 'images/projects/project-3.png',
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
+    links: { live: '#', source: '#' },
+  },
+  {
+    name: 'Multi-Post Stories',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required',
+    descriptions: `Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essent`,
+    image: 'images/projects/project-4.png',
+    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
+    links: { live: '#', source: '#' },
+  },
+];
+const works = document.querySelector('.works');
 
-              
-
-
-
-
-
-    windowPopup.style.display = 'flex';
-    navigationBarClose.style.display = 'none';
-    document.body.style.overflow = 'hidden';
-    popup.innerHTML = popupContent;
+for (let index = 0; index < projects.length; index++) {
+  const { name, description, image, technologies } = projects[index];
+  const pSection = document.createElement('section');
+  const pClasses = document.createAttribute('class');
+  let classReverse = '';
+  if ((index + 1) % 2 === 0) {
+    classReverse = 'reverse';
   }
-  function closePopupFunc(status) {
-    if (status === null) return;
-    windowPopup.style.display = 'none';
-    navigationBarClose.style.display = 'flex';
-    document.body.style.overflow = 'scroll';
+  pClasses.value = `grid-items project-1 fonts ${classReverse}`;
+  pSection.setAttributeNode(pClasses);
+  const img = document.createElement('img');
+  const imgClasses = document.createAttribute('class');
+  imgClasses.value = 'project-img';
+  img.setAttributeNode(imgClasses);
+  const imgId = document.createAttribute('id');
+  imgId.value = `project-${index + 1}`;
+  img.setAttributeNode(imgId);
+  const imgSrc = document.createAttribute('src');
+  imgSrc.value = image;
+  img.setAttributeNode(imgSrc);
+  const imgAlt = document.createAttribute('alt');
+  imgAlt.value = `Project-${index + 1}`;
+  img.setAttributeNode(imgAlt);
+  pSection.appendChild(img);
+  const cdiv = document.createElement('div');
+  const cdivClass = document.createAttribute('class');
+  cdivClass.value = 'details';
+  cdiv.setAttributeNode(cdivClass);
+  const titles = `<div class="titles">
+              <h3 class="project-title">${name}</h3>
+              <ul class="canopy">
+                <li>CANOPY</li>
+                <li class="bullets"><span class="dot"></span></li>
+                <li class="o-li">Back End Dev</li>
+                <li class="bullets"><span class="dot"></span></li>
+                <li class="o-li">2015</li>
+              </ul>
+            </div>`;
+  const text = `<p>${description}</p>`;
+  let li = '';
+  for (let i = 0; i < 3; i++) {
+    li += `<li> ${technologies[i]} </li>`;
   }
-  function worksContainer() {
-    const element = projects.map((val, index) => {
-      const cards = index % 2 === 0 ? `<div class="container">
-    <img class="work-pic" src="./img/desktop/work1.svg" alt="nature image" />
-    <img class="work-pic" src=${val.pic} alt="landing page view" />
-    <div class="work-description">
-        <h4>${val.name}</h4>
-        <ul class="description-element">
-            <li>${val.companyId}</li>
-            <li class="element">
-                <img class="bullet" src="./images/bullet.png" alt="bullet" />
-            </li>
-            <li class="element">${val.work}</li>
-            <li class="element">
-                <img class="bullet" src="./images/bullet.png" alt="bullet" />
-            </li>
-            <li class="element">${val.date}</li>
+  const ul = `<ul class="categories hex"> ${li} </ul>`;
+
+  const seeBtn = `<div class="see-project">
+              <a href="#"
+                ><button type="button" class="hex project-details" data-project="${index}">
+                  See Project
+                </button></a
+              >
+            </div>`;
+  cdiv.innerHTML = titles + text + ul + seeBtn;
+
+  pSection.appendChild(cdiv);
+
+  works.appendChild(pSection);
+}
+const seeProject = document.querySelectorAll('.project-details');
+const main = document.querySelector('#main');
+function closePopup() {
+  document.querySelector('#main').classList.remove('main');
+  document.querySelector('#popup').classList.remove('popup');
+  document.querySelector('#popup').classList.add('hidden-popup');
+}
+seeProject.forEach((element) => {
+  element.addEventListener('click', () => {
+    document.querySelector('#popup').classList.remove('hidden-popup');
+    document.querySelector('#popup').classList.add('popup');
+    main.classList = 'main';
+    // eslint-disable-next-line operator-linebreak, object-curly-newline
+    const { name, descriptions, image, technologies, links } =
+      projects[element.dataset.project];
+    const pTitles = `<div class="p-title">
+        <div class="p-main-title">
+          <h3 class="p-h3">${name}</h3>
+          <p id="btn-close">X</p>
+        </div>
+        <ul class="p-canopy">
+          <li>CANOPY</li>
+          <li class="p-bullets"><span class="p-dot"></span></li>
+          <li class="o-li">Back End Dev</li>
+          <li class="p-bullets"><span class="p-dot"></span></li>
+          <li class="o-li">2015</li>
         </ul>
-        <p class="description-text">${val.description}</p>
-        <ul class="languages">
-         ${val.techUse.map((el) => `<li class="lang">${el}</li>`).join('')}
-        </ul><br>
-  
-        <button class="btn openPopup" id="firstBtn" type="submit" onclick="popupFunc(${val.id})">See Project</button>
-    </div>
-  </div>`
-        : `            <div class="container">
-                  <img class="work-pic" src="./images/Portoflio_Card/work1.png" alt="multi-post stories image" />
-                  <div class="work-description">
-                      <h4>${val.name}</h4>
-                      <ul class="description-element">
-                          <li>${val.companyId}</li>
-                          <li class="element">
-                              <img class="bullet" src="./images/bullet.png" alt="bullet" />
-                          </li>
-                          <li class="element">${val.work}</li>
-                          <li class="element">
-                              <img class="bullet" src="./images/bullet.png" alt="bullet" />
-                          </li>
-                          <li class="element">${val.date}</li>
-                      </ul>
-                      <p class="description-text">${val.description}</p>
-                      <ul class="languages">
-                          ${val.techUse.map((el) => `<li class="lang">${el}</li>`).join('')}
-                      </ul><br>
-                      <button class="btn openPopup" id="secondBtn" onclick="popupFunc(${val.id})" type="submit">See
-                          Project</button>
-                  </div>
-                  <img class="work-pic-1" src=${val.pic} alt="professional art printing image" />
-              </div>`;
-      return cards;
-    });
-    return element;
-  }
-  
-  window.addEventListener('load', () => {
-    mainContainer.innerHTML = worksContainer();
-    return mainContainer;
+      </div>
+
+      <img
+        class="p-project-img"
+        id="project-${Number(element.dataset.project) + 1}"
+        src="${image}"
+        alt="Project-1"
+      />`;
+    let li = '';
+    for (let i = 0; i < technologies.length; i++) {
+      if (i >= 3) {
+        li += `<li class="categories-li mibele-hidden"> ${technologies[i]} </li>`;
+      } else {
+        li += `<li class="categories-li"> ${technologies[i]} </li>`;
+      }
+    }
+    const { live, source } = links;
+    const detailss = `<div class="desktop">
+        <p class="p-text">
+          ${descriptions}
+        </p>
+        <div class="desktop-right">
+          <ul class="p-categories hex">
+          ${li}
+          </ul>
+          <div class="p-see-project">
+            <a href="${live}"><button type="button" class="btn-see hex">
+              <span class="btn-text">See live</span
+              ><img alt="Live" src="images/socials/live.png" />
+            </button></a>
+            <a href="${source}"><button type="button" class="btn-see hex">
+              <span class="btn-text">See Source</span
+              ><img alt="Live" src="images/socials/github.png" />
+            </button></a>
+          </div>
+        </div>
+      </div>`;
+    document.querySelector('#popup').innerHTML = pTitles + detailss;
+    document.querySelector('#btn-close').addEventListener('click', closePopup);
   });
-  popupFunc(null);
-  closePopupFunc(null);
+});
