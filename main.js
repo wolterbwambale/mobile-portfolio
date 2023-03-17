42f847f270625d918b4a64d18daec1e6aac69834
wolterbwambale <benplaan1@gmail.com>
2023-03-17 23:37:36 +0300

    fixed linters

--- main.js
+++ main.js
@@ -21,25 +21,25 @@ function showMessage(message) {
     const msg = document.querySelector('small');
     msg.innerText = message;
   }
-  
+
   function showError(message) {
     showMessage(message);
   }
-  
+
   function showSuccess() {
     showMessage('');
   }
-  
+
   function validateEmail(input, invalidMsg) {
     const emailRegex = /[A-Z]/;
-  
+
     const email = input.value.trim();
     if (emailRegex.test(email)) {
       return showError(invalidMsg);
     }
     return true;
   }
-  
+
   const EMAIL_INVALID = 'Please enter your email address in lower case';
   const form = document.querySelector('form');
   form.addEventListener('submit', (event) => {
